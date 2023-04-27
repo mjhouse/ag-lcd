1e4c4e255e0aaa041af77e80ffe8a7c0dc3b803c
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-04-27 15:42:08 +0300

    Add some docs to new methods

--- src/i2c.rs
+++ src/i2c.rs
@@ -47,6 +47,8 @@ where
     I2C: I2cBus,
     <I2C as I2cBus>::BusError: Debug,
 {
+    /// Descructs pin collection from port expander and constructs LcdDisplay using pins that are
+    /// available.
     fn from_parts(parts: pcf8574::Parts<'a, I2C, M>, delay: D) -> Self {
         let pcf8574::Parts {
             p0,
@@ -74,12 +76,54 @@ where
     }
 
     /// Creates a new [`LcdDisplay`] using PCF8572A for interfacing
+    ///
+    /// Refer to [Pcf8574a docs](https://docs.rs/port-expander/latest/port_expander/dev/pcf8574/struct.Pcf8574a.html) from crate `port-expander` for more information about setup of the port expander
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let peripherals = arduino_hal::Peripherals::take().unwrap();
+    /// let pins = arduino_hal::pins!(peripherals);
+    /// let delay = arduino_hal::Delay::new();
+    ///
+    /// let sda = pins.a4.into_pull_up_input();
+    /// let scl = pins.a5.into_oull_up_input();
+    ///
+    /// let i2c_bus = arduino_hal::i2c::I2c::new(dp.TWI, sda, scl, 50000);
+    /// let mut i2c_expander = Pcf8574a::new(i2c_bus, true, true, true);
+    ///
+    /// let mut lcd: LcdDisplay<_,_> = LcdDisplay::new_pcf8574a(&mut i2c_expander, delay)
+    ///     .with_blink(Blink::On)
+    ///     .with_cursor(Cursor::Off)
+    ///     .build();
+    /// ```
     #[inline]
     pub fn new_pcf8574a(expander: &'a mut Pcf8574a<M>, delay: D) -> Self {
         Self::from_parts(expander.split(), delay)
     }
 
     /// Creates a new [`LcdDisplay`] using PCF8572 for interfacing
+    ///
+    /// Refer to [Pcf8574a docs](https://docs.rs/port-expander/latest/port_expander/dev/pcf8574/struct.Pcf8574.html) from crate `port-expander` for more information about setup of the port expander
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let peripherals = arduino_hal::Peripherals::take().unwrap();
+    /// let pins = arduino_hal::pins!(peripherals);
+    /// let delay = arduino_hal::Delay::new();
+    ///
+    /// let sda = pins.a4.into_pull_up_input();
+    /// let scl = pins.a5.into_oull_up_input();
+    ///
+    /// let i2c_bus = arduino_hal::i2c::I2c::new(dp.TWI, sda, scl, 50000);
+    /// let mut i2c_expander = Pcf8574::new(i2c_bus, true, true, true);
+    ///
+    /// let mut lcd: LcdDisplay<_,_> = LcdDisplay::new_pcf8574a(&mut i2c_expander, delay)
+    ///     .with_blink(Blink::On)
+    ///     .with_cursor(Cursor::Off)
+    ///     .build();
+    /// ```
     #[inline]
     pub fn new_pcf8574(expander: &'a mut Pcf8574<M>, delay: D) -> Self {
         Self::from_parts(expander.split(), delay)
